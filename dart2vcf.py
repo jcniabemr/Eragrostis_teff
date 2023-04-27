2bdf8c514af4773895287ad75e38c852b92e9a14
jcniabemr <john.connell@niab.com>
2023-04-27 19:39:05 +0100

    update

--- dart2vcf.py
+++ dart2vcf.py
@@ -12,6 +12,7 @@ parse=ap.parse_args()
 
 ####Create lists
 out_file_info=[]
+position=[]
 
 ####Read in data 
 df=pd.read_csv(parse.i,skiprows=6,low_memory=False)
@@ -21,22 +22,36 @@ df = df.drop(columns=['CloneID','SnpPosition','AlleleSequenceRef','AlleleSequenc
 
 ####Duplicate this col for later 
 df['second_snp'] = df.loc[:, 'SNP']
+df['FILTER']='PASS'
+df['INFO']='.'
+df['FORMAT']='GT'
 
 ####Re-order cols 
 cols = list(df.columns)
-cols = [cols[-1]] + cols[:-1]
+cols = cols[-4:] + cols[:-4]
 df = df[cols]
-def swap_columns(df,a,b,c,d,e,f):
+
+def swap_columns(df,a,b,c,d,e,f,g,h,i):
     col_list = list(df.columns)
-    t,u,v,w,x,y = col_list.index(a),col_list.index(b),col_list.index(c),col_list.index(d),col_list.index(e),col_list.index(f)
-    col_list[t],col_list[u],col_list[v],col_list[w],col_list[x],col_list[y]=col_list[v],col_list[w],col_list[u],col_list[y],col_list[t],col_list[x]
+    r,s,t,u,v,w,x,y,z = col_list.index(a),col_list.index(b),col_list.index(c),col_list.index(d),col_list.index(e),col_list.index(f),col_list.index(g),col_list.index(h),col_list.index(i)
+    col_list[r],col_list[s],col_list[t],col_list[u],col_list[v],col_list[w],col_list[x],col_list[y],col_list[z] = col_list[w],col_list[x],col_list[v],col_list[z],col_list[r],col_list[y],col_list[s],col_list[t],col_list[u]
     df = df[col_list]
     return df
-df = swap_columns(df,'second_snp','AlleleID','Chrom_Eragrostis_CogeV3','ChromPosSnp_Eragrostis_CogeV3','AlnEvalue_Eragrostis_CogeV3','SNP')
+df = swap_columns(df,'second_snp','FILTER','INFO','FORMAT','AlleleID','Chrom_Eragrostis_CogeV3','ChromPosSnp_Eragrostis_CogeV3','AlnEvalue_Eragrostis_CogeV3','SNP')
 
 ####Convert back to list for further editing 
 reformed=df.T.reset_index().values.T.tolist()
+
+####Create set of duplicated positions 
 for x in reformed:
+	position.append(str(x[0])+"_"+str(x[1]))
+duplicates = set(x for x in position if position.count(x) > 1)
+
+####Edit new reformed data to be consistent with VCF
+for x in reformed:
+	a="\t".join(map(str,x))
+	if a.startswith("nan"): 
+		continue 
 	true_id=x[2].split("|")[0]
 	x[2]=x[2].replace(x[2],true_id)
 	true_ref=x[3].split(">")[0][-1]
@@ -55,17 +70,25 @@ for x in reformed:
 		x[4]=x[4].replace(x[4],"ALT")
 	if x[5] == "AlnEvalue_Eragrostis_CogeV3":
 		x[5]=x[5].replace(x[5],"QUAL")
-	for i in range(6,250):
+	x[5]=str(x[5])
+	if x[5] != "QUAL":
+		x[5]=x[5].replace(x[5], "999")	
+	for i in range(9,253):
 		if x[i] == "0":
 			x[i]=x[i].replace(x[i],"0/0")
 		elif x[i] == "1":
-			x[i]=x[i].replace(x[i],"1/1")
-		elif x[i] == "2":
 			x[i]=x[i].replace(x[i],"0/1")
+		elif x[i] == "2":
+			x[i]=x[i].replace(x[i],"1/1")
 		elif x[i] == "-":
 			x[i]=x[i].replace(x[i],"./.")
-	out_file_info.append("\t".join(map(str,x)))
-
+	if str(x[0])+"_"+str(x[1]) not in duplicates:
+		out_file_info.append("\t".join(map(str,x)))
+	
+####Write file 
 with open("dart_mapping.vcf", 'w') as f:
 	for x in out_file_info:
 		f.write(f"{x}\n")
+
+
+
--- dartseq_analysis.sh
+++ dartseq_analysis.sh
@@ -17,7 +17,7 @@ cat	teff.Dabbi_50954_V3_kora_asgori.vcf | grep -v "INDEL" > SNP_teff.Dabbi_50954
 python /home/jconnell/git_repos/niab_repos/eragrostis/extract_vcf_info.py -snp SNP_teff.Dabbi_50954_V3_kora_asgori.vcf -indel INDEL_teff.Dabbi_50954_V3_kora_asgori.vcf
 
 ####Exclude SNPs round INDELs
-vcftools --vcf SNP_teff.Dabbi_50954_V3_kora_asgori.vcf --exclude-bed snp_excluson_data.txt --keep-INFO-all --recode --stdout > filtered_indel_SNP_teff.Dabbi_50954_V3_kora_asgori.vcf
+vcftools --vcf SNP_teff.Dabbi_50954_V3_kora_asgori.vcf --exclude-bed snp_excluson_data.txt --recode --recode-INFO-all --out filtered_indel_SNP_teff.Dabbi_50954_V3_kora_asgori
 
 ####Split up VCF by sample 
 for x in $(echo $(bcftools query -l filtered_indel_SNP_teff.Dabbi_50954_V3_kora_asgori.vcf)); do
@@ -27,11 +27,27 @@ done
 ####Apply depth and qual filters to individual files 
 #vcftools --vcf Asgori-Tank1_SNP_indelfiltered.vcf --minDP 25 --maxDP 140 --keep-INFO-all --recode --stdout > Asgori-Tank1_SNP_indelfiltered_depthfilter.vcf
 
-bcftools filter -O v -o Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.vcf -i 'QUAL >= 20 & INFO/DP <= 140 && INFO/DP >= 25' Asgori-Tank1_SNP_indelfiltered.vcf
-bcftools filter -O v -o Kora-Tank1_SNP_indelfiltered_depth_qualfilter.vcf -i 'QUAL >= 20 & INFO/DP <= 150 && INFO/DP >= 40' Kora-Tank1_SNP_indelfiltered.vcf
+#bcftools filter -O v -o Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.vcf -i 'QUAL >= 20 & INFO/DP <= 140 && INFO/DP >= 25' Asgori-Tank1_SNP_indelfiltered.vcf
+#bcftools filter -O v -o Kora-Tank1_SNP_indelfiltered_depth_qualfilter.vcf -i 'QUAL >= 20 & INFO/DP <= 150 && INFO/DP >= 40' Kora-Tank1_SNP_indelfiltered.vcf
 
 # #For fitlering bcftools uses "&" to keep vairiants that meet all given criteria and "&&" to keep variants that match at least one of the given criteria
 # https://www.htslib.org/workflow/filter.html
 # ####Filter1 - based on quality, a score of >20 means that there is a 99% probability there is a variant at that site 
 # bcftools filter -O v -o filter_SNP_teff.Dabbi_50954_V3_kora_asgori.vcf -i 'QUAL >= 10 & INFO/DP <= 320 & MQBZ >=-5 && MQBZ <= 5 && MQSBZ >=-5 && MQSBZ <= 5 & RPBZ >= -7.5 && RPBZ <= 7.5 & SCBZ > 4' SNP_teff.Dabbi_50954_V3_kora_asgori.vcf
+
+vcftools --vcf Asgori-Tank1_SNP_indelfiltered.vcf --minQ 20 --minDP 25 --maxDP 140  --recode --recode-INFO-all --out Asgori-Tank1_SNP_indelfiltered_depth_qualfilter
+vcftools --vcf Kora-Tank1_SNP_indelfiltered.vcf --minQ 20 --minDP 40 --maxDP 150 --recode --recode-INFO-all --out Kora-Tank1_SNP_indelfiltered_depth_qualfilter
+
+####Merge vcf files 
+bcftools view Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf -Oz -o Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz
+bcftools view Kora-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf -Oz -o Kora-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz
+bcftools index Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz
+bcftools index Kora-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz
+bcftools merge Asgori-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz Kora-Tank1_SNP_indelfiltered_depth_qualfilter.recode.vcf.gz > Asgori-Tank1_Kora-Tank1_filtered_combined.vcf
+
+####Remove cases of only one genotype 
+vcftools --vcf Asgori-Tank1_Kora-Tank1_filtered_combined.vcf --max-missing 1.0 --recode --recode-INFO-all --out unique_Asgori-Tank1_Kora-Tank1_filtered_combined.vcf
+
+####Remove duplicated positions and positions equal to 0
+cat dart_mapping.vcf | awk '($2 > 0){print $0}' > tmp && mv tmp	dart_mapping.vcf
 
